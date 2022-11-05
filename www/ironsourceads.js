a9b19e45ac542cd2481a1092d8c31eb1700c9c78
Milos Stevanovic <milos@mstevanovic.net>
2022-11-05 23:47:25 +0100

    Reformat ironsourceads.js

--- www/ironsourceads.js
+++ www/ironsourceads.js
@@ -1,175 +1,177 @@
 var IronSourceAds = (function () {
-   var initialized = false;
+	var initialized = false;
 
-   return {
-      /**
-       * Returns the state of initialization
-       */
-      isInitialized: function isInitialized() {
-         return initialized;
-      },
+	return {
 
-      /**
-       * Sets the user id, used for Offerwall ad units or using
-       * server-to-server callbacks to reward your users with our rewarded ad units
-       * @param {String} - user id
-       */
-      setDynamicUserId: function setUserId(params) {
-         params = defaults(params, {});
+		/**
+		 * Returns the state of initialization
+		 */
+		isInitialized: function isInitialized() {
+			return initialized;
+		},
 
-         if (params.hasOwnProperty("userId") === false) {
-            throw new Error("IronSourceAds::setUserId - missing userId IronSourceAds.setUserId({userId:'example'})");
-         }
+		/**
+		 * Sets the user id, used for Offerwall ad units or using
+		 * server-to-server callbacks to reward your users with our rewarded ad units
+		 * @param {String} - user id
+		 */
+		setDynamicUserId: function setUserId(params) {
+			params = defaults(params, {});
 
-         callPlugin("setDynamicUserId", [params.userId], params.onSuccess, params.onFailure);
-      },
+			if (params.hasOwnProperty('userId') === false) {
+				throw new Error("IronSourceAds::setUserId - missing userId IronSourceAds.setUserId({userId:'example'})");
+			}
 
-      /**
-       * Validate Integration
-       * @param {Function} params.onSuccess - optional on success callback
-       * @param {Function} params.onFailure - optional on failure callback
-       */
-      validateIntegration: function validateIntegration(params) {
-         params = defaults(params, {});
+			callPlugin('setDynamicUserId', [params.userId], params.onSuccess, params.onFailure);
+		},
 
-         callPlugin("validateIntegration", [], params.onSuccess, params.onFailure);
-      },
+		/**
+		 * Validate Integration
+		 * @param {Function} params.onSuccess - optional on success callback
+		 * @param {Function} params.onFailure - optional on failure callback
+		 */
+		validateIntegration: function validateIntegration(params) {
+			params = defaults(params, {});
 
-      /**
-       * Initializes iron source
-       * @param {Function} params.onSuccess - optional on success callback
-       */
-      init: function init(params) {
-         params = defaults(params, { userId: "" });
+			callPlugin('validateIntegration', [], params.onSuccess, params.onFailure);
+		},
 
-         if (params.hasOwnProperty("appKey") === false) {
-            throw new Error("IronSourceAds::init - appKey is required");
-         }
+		/**
+		 * Initializes iron source
+		 * @param {Function} params.onSuccess - optional on success callback
+		 */
+		init: function init(params) {
+			params = defaults(params, {userId: ''});
 
-        return new Promise((resolve, reject) => {
-            callPlugin('init', [params.appKey, params.userId], function () {
+			if (params.hasOwnProperty('appKey') === false) {
+				throw new Error('IronSourceAds::init - appKey is required');
+			}
+			return new Promise((resolve, reject) => {
+				callPlugin('init', [params.appKey, params.userId], function () {
 
-                initialized = true;
+					initialized = true;
 
-                if (isFunction(params.onSuccess)) {
-                    params.onSuccess();
-                }
-                resolve(true);
+					if (isFunction(params.onSuccess)) {
+						params.onSuccess();
+					}
+					resolve(true);
 
-            }, () => {
-                if (isFunction(params.onFailure)) {
-                    params.onFailure()
-                }
+				}, (...args) => {
+					if (isFunction(params.onFailure)) {
+						params.onFailure()
+					}
 
-                reject("Failed to initialize ironsource");
-            });
-        });
-      },
+					reject("Failed to initialize ironsource");
+				});
+			});
+		},
 
-      /**
-       * Shows rewarded video
-       * @param {String} params.placement - optional placement name
-       * @param {Function} params.onSuccess - optional on success callback
-       * @param {Function} param.onFailure - optional on failure callback
-       */
-      showRewardedVideo: function showRewardedVideo(params) {
-         params = defaults(params, { placement: "default" });
+		/**
+		 * Shows rewarded video
+		 * @param {String} params.placement - optional placement name
+		 * @param {Function} params.onSuccess - optional on success callback
+		 * @param {Function} param.onFailure - optional on failure callback
+		 */
+		showRewardedVideo: function showRewardedVideo(params) {
+			params = defaults(params, {placement: 'default'});
 
-         callPlugin("showRewardedVideo", [params.placement], params.onSuccess, params.onFailure);
-      },
+			callPlugin('showRewardedVideo', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Checks if rewarded video is available
-       * @param {Function} params.onSuccess - function to call the result to
-       */
-      hasRewardedVideo: function hasRewardedVideo(params) {
-         params = defaults(params, {});
+		/**
+		 * Checks if rewarded video is available
+		 * @param {Function} params.onSuccess - function to call the result to
+		 */
+		hasRewardedVideo: function hasRewardedVideo(params) {
+			params = defaults(params, {});
 
-         if (isFunction(params.onSuccess) === false) {
-            throw new Error("IronSourceAdsPlugin::hasRewardedVideo expects onSuccess");
-         }
+			if (isFunction(params.onSuccess) === false) {
+				throw new Error('IronSourceAdsPlugin::hasRewardedVideo expects onSuccess');
+			}
 
-         callPlugin("hasRewardedVideo", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('hasRewardedVideo', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Shows banner if avaialble
-       * @param {Function} params.onSuccess
-       */
-      showBanner: function showBanner(params) {
-         params = defaults(params, {});
+		/**
+		 * Shows banner if avaialble
+		 * @param {Function} params.onSuccess
+		 */
+		showBanner: function showBanner(params) {
+			params = defaults(params, {});
 
-         callPlugin("showBanner", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('showBanner', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Shows banner if avaialble
-       * @param {Function} params.onSuccess
-       */
-      hideBanner: function showBanner(params) {
-         params = defaults(params, {});
+		/**
+		 * Shows banner if avaialble
+		 * @param {Function} params.onSuccess
+		 */
+		hideBanner: function showBanner(params) {
+			params = defaults(params, {});
 
-         callPlugin("hideBanner", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('hideBanner', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Checks if offerwall is available
-       * @param {Function} params.onSuccess - function to call the result to
-       */
-      hasOfferwall: function hasOfferwall(params) {
-         params = defaults(params, {});
+		/**
+		 * Checks if offerwall is available
+		 * @param {Function} params.onSuccess - function to call the result to
+		 */
+		hasOfferwall: function hasOfferwall(params) {
+			params = defaults(params, {});
 
-         callPlugin("hasOfferwall", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('hasOfferwall', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Shows the offerwall if available
-       * @param {Function} params.onSuccess
-       */
-      showOfferwall: function showOfferwall(params) {
-         params = defaults(params, {});
+		/**
+		 * Shows the offerwall if available
+		 * @param {Function} params.onSuccess
+		 */
+		showOfferwall: function showOfferwall(params) {
+			params = defaults(params, {});
 
-         callPlugin("showOfferwall", [params.placement], params.onSuccess, params.onFailure);
-      },
+			callPlugin('showOfferwall', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Loads interstitial
-       */
-      loadBanner: function loadBanner(params) {
-         params = defaults(params, {});
+		/**
+		 * Loads interstitial
+		 */
+		loadBanner: function loadBanner(params) {
+			params = defaults(params, {});
 
-         callPlugin("loadBanner", [params.placement, params.size, params.position], params.onSuccess, params.onFailure);
-      },
+			callPlugin('loadBanner', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Loads interstitial
-       */
-      loadInterstitial: function loadInterstitial(params) {
-         params = defaults(params, {});
+		/**
+		 * Loads interstitial
+		 */
+		loadInterstitial: function loadInterstitial(params) {
+			params = defaults(params, {});
 
-         callPlugin("loadInterstitial", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('loadInterstitial', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Show interstitial
-       */
-      showInterstitial: function showInterstitial(params) {
-         params = defaults(params, {});
+		/**
+		 * Show interstitial
+		 */
+		showInterstitial: function showInterstitial(params) {
+			params = defaults(params, {});
 
-         callPlugin("showInterstitial", [], params.onSuccess, params.onFailure);
-      },
+			callPlugin('showInterstitial', [], params.onSuccess, params.onFailure);
+		},
 
-      /**
-       * Checks to see if interstitial is loaded
-       * @param {Function} params.onSuccess
-       */
-      hasInterstitial: function isInterstitialReady(params) {
-         params = defaults(params, {});
+		/**
+		 * Checks to see if interstitial is loaded
+		 * @param {Function} params.onSuccess
+		 */
+		hasInterstitial: function isInterstitialReady(params) {
+			params = defaults(params, {});
 
-         callPlugin("hasInterstitial", [], params.onSuccess, params.onFailure);
-      },
-   };
+			callPlugin('hasInterstitial', [], params.onSuccess, params.onFailure);
+		}
+
+	}
 })();
+
 
 /**
  * Helper function to call cordova plugin
@@ -179,21 +181,16 @@ var IronSourceAds = (function () {
  * @param {Function} onFailure - optional on failure functioin
  */
 function callPlugin(name, params, onSuccess, onFailure) {
-   cordova.exec(
-      function callPluginSuccess(result) {
-         if (isFunction(onSuccess)) {
-            onSuccess(result);
-         }
-      },
-      function callPluginFailure(error) {
-         if (isFunction(onFailure)) {
-            onFailure(error);
-         }
-      },
-      "IronSourceAdsPlugin",
-      name,
-      params
-   );
+	cordova.exec(function callPluginSuccess(result) {
+
+		if (isFunction(onSuccess)) {
+			onSuccess(result);
+		}
+	}, function callPluginFailure(error) {
+		if (isFunction(onFailure)) {
+			onFailure(error)
+		}
+	}, 'IronSourceAdsPlugin', name, params);
 }
 
 /**
@@ -201,9 +198,9 @@ function callPlugin(name, params, onSuccess, onFailure) {
  * @param {Object} functionToCheck - function to check if is function
  */
 function isFunction(functionToCheck) {
-   var getType = {};
-   var isFunction = functionToCheck && getType.toString.call(functionToCheck) === "[object Function]";
-   return isFunction === true;
+	var getType = {};
+	var isFunction = functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+	return isFunction === true;
 }
 
 /**
@@ -212,19 +209,20 @@ function isFunction(functionToCheck) {
  * @param {Object} defaultObject - defaults to extend o with
  */
 function defaults(o, defaultObject) {
-   if (typeof o === "undefined") {
-      return defaults({}, defaultObject);
-   }
+	if (typeof o === 'undefined') {
+		return defaults({}, defaultObject);
+	}
 
-   for (var j in defaultObject) {
-      if (defaultObject.hasOwnProperty(j) && o.hasOwnProperty(j) === false) {
-         o[j] = defaultObject[j];
-      }
-   }
+	for (var j in defaultObject) {
+		if (defaultObject.hasOwnProperty(j) && o.hasOwnProperty(j) === false) {
+			o[j] = defaultObject[j];
+		}
+	}
 
-   return o;
+	return o;
 }
 
+
 if (typeof module !== undefined && module.exports) {
-   module.exports = IronSourceAds;
+	module.exports = IronSourceAds;
 }
